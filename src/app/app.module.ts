f20fe0ce692b1d6b715ab2e251f418632b313c0f
Luis H. Moreno <luishmcmoreno@gmail.com>
2017-11-03 02:35:11 -0200

    including dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { BdmgProvider } from '../providers/bdmg/bdmg';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BdmgProvider
   ]
 })
 export class AppModule {}
